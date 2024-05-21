0fcf1efad371c594e42023674682d64bbcba1b3d
rusip <rusipbox@gmail.com>
2024-05-22 02:40:07 +0300

    created the basic models for social media in social_media/models.py

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+from social_media_platform import settings
+
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-date_posted"]
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.user}: {self.content}"
+
+    
+class Like(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("user", "post"),)
+        ordering = ["-user"]
+
+    def __str__(self):
+        return f"{self.user} - {self.post}"
+
+
+class Dislike(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = (("user", "post"),)
+        ordering = ["-user"]
+
+    def __str__(self):
+        return f"{self.user} - {self.post}"
