fc8c3903979f02ed66833645c52c1e104c823e06
rusip <rusipbox@gmail.com>
2024-05-22 02:12:00 +0300

    registered User in user/admin.py

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext as gt
 
-# Register your models here.
+from user.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    fieldsets = [
+        (None,
+         {
+             'fields': (
+                 'username', 'first_name', 'last_name', 'email', 'password'
+             )
+         }
+         ),
+        (
+            gt("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions"
+                )
+            }
+        ),
+        (gt("Important dates"), {"fields": ("last_login", "date_joined")}),
+    ]
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("first_name", "last_name", "email", "password")
+            },
+        ),
+    )
+    list_display = (
+        "first_name", "last_name", "email", "password", "is_active"
+    )
+    search_fields = ("username", "first_name", "last_name", "email")
+    ordering = ("username",)
