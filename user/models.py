e73b23f9839e9fa02dae748d320ad1f18580e62f
rusip <rusipbox@gmail.com>
2024-05-22 01:44:07 +0300

    created User model in user/models.py

--- user/models.py
+++ user/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext as _
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault("is_staff", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_staff", True)
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = models.CharField(_("username"), max_length=50, unique=True)
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=50)
+    last_name = models.CharField(_("last name"), max_length=50)
+    password = models.CharField(_("password"), max_length=30)
+
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
+
+    REQUIRED_FIELDS = [
+        "username", "email", "first_name", "last_name", "password"
+    ]
+
+    objects = UserManager()
